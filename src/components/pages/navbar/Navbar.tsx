613328707ff8bbd37fafb835db081fe8a6d70520
w0wayush <ayupandey82@gmail.com>
2024-10-30 23:39:01 +0530

    Made the navbar more responsive

--- src/components/pages/navbar/Navbar.tsx
+++ src/components/pages/navbar/Navbar.tsx
@@ -44,42 +44,39 @@ const Navbar = () => {
 
   return (
     <div className="border-b-2 rounded-b-md p-4 border-slate-700 px-10">
-      {/* FlowZone Logo */}
-      <div className="flex justify-between items-center ">
+      <div className="flex justify-between items-center">
         <div
           className="flex items-center gap-1 cursor-pointer"
           onClick={() => navigate.push("/")}
         >
-          <Image src={Flow} alt="Flow Zone Logo" className="h-16 w-16"></Image>
-          {/* <h1>FlowZone</h1> */}
+          <Image src={Flow} alt="Flow Zone Logo" className="h-16 w-16" />
           <h1 className="text-lg font-bold">TaskFlow</h1>
         </div>
 
-        {/* Sign-in / Sign-out and User Icon along with dark mode / light mode*/}
         <div className="flex">
           {user && (
             <div className="flex items-center mr-4">
-              <p>Welcome, {user.username}!</p>
+              <p className="sm:flex">
+                <span className="sm:block hidden">Welcome, </span>
+                {user.username}!
+              </p>
             </div>
           )}
-          <div className="">
+          <div className="sm:block hidden">
             <Theme />
           </div>
           <div>
             <div className="flex">
               {user ? (
-                //   {isLoggedIn ? (
                 <div className="flex items-center justify-center relative">
-                  {/* Show users icon  */}
                   <div
                     className="flex items-center justify-center border-2 ml-4 h-10 w-10 rounded-full hover:bg-gray-100 cursor-pointer"
                     onClick={handleToggle}
                   >
                     <FaUser className="text-lg text-gray-500" />
                   </div>
-                  {/* Dropdown menu that will be shown on click */}
                   {isOpen && (
-                    <div className="absolute top-full right-0 mt-2 w-40 bg-slate-900  text-white font-medium rounded-md border-gray border-2 z-10">
+                    <div className="absolute top-full right-0 mt-2 w-40 bg-slate-900 text-white font-medium rounded-md border-gray border-2 z-10">
                       <ul>
                         <li
                           className="py-2 px-4 hover:bg-slate-700 cursor-pointer"
@@ -88,14 +85,11 @@ const Navbar = () => {
                             navigate.push("/profile");
                           }}
                         >
-                          <div className="flex items-center  gap-4">
+                          <div className="flex items-center gap-4">
                             <CgProfile size={20} />
                             <button>Profile</button>
                           </div>
                         </li>
-                      </ul>
-                      <div className="border-2 "></div>
-                      <ul>
                         <li
                           className="py-2 px-4 hover:bg-slate-700 cursor-pointer"
                           onClick={() => {
@@ -103,16 +97,17 @@ const Navbar = () => {
                             navigate.push("/kanban");
                           }}
                         >
-                          <div className="flex items-center  gap-4">
+                          <div className="flex items-center gap-4">
                             <MdOutlineSpaceDashboard size={20} />
-                            <button onClick={() => navigate.push("/kanban")}>
-                              Dashboard
-                            </button>
+                            <button>Dashboard</button>
                           </div>
                         </li>
-                      </ul>
-                      <div className="border-2 "></div>
-                      <ul>
+                        <li className="block sm:hidden relative">
+                          <div className="flex items-center gap-4">
+                            <Theme />
+                            <div className="absolute left-12">Theme</div>
+                          </div>
+                        </li>
                         <li
                           className="py-2 px-4 hover:bg-slate-700 cursor-pointer"
                           onClick={() => {
@@ -120,7 +115,7 @@ const Navbar = () => {
                             handleLogout();
                           }}
                         >
-                          <div className="flex items-center  gap-4">
+                          <div className="flex items-center gap-4">
                             <FiLogOut size={20} />
                             <button>Logout</button>
                           </div>
@@ -131,25 +126,21 @@ const Navbar = () => {
                 </div>
               ) : (
                 <div>
-                  {/* Create Account Button for small screens */}
                   <Link href={"/login"}>
                     <Button
                       variant="default"
-                      className="lg:hidden block px-4 py-2 text-sm font-medium  ml-4 flex"
-                      // onClick={() => route.push("/login")}
+                      className="lg:hidden block px-4 py-2 text-sm font-medium ml-4 flex"
                     >
-                      <FaUser className="mr-2 " />
+                      <FaUser className="mr-2" />
                       Create Account
                     </Button>
                   </Link>
-                  {/* Login / Register Button for Large screens */}
                   <Link href={"/login"}>
                     <Button
                       variant="default"
-                      className="lg:block hidden lg:flex lg:px-4 lg:py-2 lg:text-sm lg:font-medium  lg:ml-4"
-                      //   onClick={() => route.push("/login")}
+                      className="lg:block hidden lg:flex lg:px-4 lg:py-2 lg:text-sm lg:font-medium lg:ml-4"
                     >
-                      <FaUser className="mr-2 " />
+                      <FaUser className="mr-2" />
                       Login / Register
                     </Button>
                   </Link>
